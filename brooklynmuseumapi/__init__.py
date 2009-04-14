d1bdd34ba96cb861371774c29c8ca2cd2449460e
Mark Matienzo <mark@matienzo.org>
2009-04-14 15:10:11 -0400

    initial import

---
+++ brooklynmuseumapi/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2009 Mark A. Matienzo
+#
+# This file is part of the brooklynmuseumapi Python module.
+#
+# worldcat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# worldcat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with worldcat.  If not, see <http://www.gnu.org/licenses/>.
+
+# __init__.py - initialize module
+
+__version__ = '0.1.0'
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = """
+Intended Audience :: Education
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Development Status :: 3 - Alpha
+"""
+
+setup(
+    name = 'brooklynmuseumapi',
+    version = '0.1.0',  # update brooklynmuseumapi/__init__.py on release!
+    url = 'http://bitbucket.org/anarchivist/brooklynmuseumapi',
+    author = 'Mark A. Matienzo',
+    author_email = 'mark@matienzo.org',
+    license = 'GPL/BSD',
+    packages = find_packages(),
+    install_requires = install_requires,
+    description = 'Interact with the Brooklyn Museum Collection API',
+    classifiers = filter(None, classifiers.split('\n')),
+)
